0b60935583d45e4786b27fbd9c715939f4efa03c
Quantum1Shadow <ua667766706@gmail.com>
2020-05-28 18:33:27 +0300

    added three tabs with navigation

--- Export/neko/haxe/ApplicationMain.hx
+++ Export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "23";
+		app.meta["build"] = "1";
 		app.meta["company"] = "Company Name";
 		app.meta["file"] = "HelloOpenfl";
 		app.meta["name"] = "HelloOpenfl";
--- Source/Main.hx
+++ Source/Main.hx
@@ -1,6 +1,5 @@
 package;
 
-import ru.stablex.ui.widgets.Slider;
 import openfl.display.Sprite;
 import openfl.Assets;
 import flash.Lib;
@@ -12,32 +11,31 @@ import ru.stablex.ui.widgets.Button;
 class Main extends Sprite {
     public static function main() {
         UIBuilder.init();
-        var vbox = UIBuilder.create(VBox);
-        var text = UIBuilder.create(Text, {
-              text         : 'Hello from openfl',
-               embededFonts: true,
-               format      : {
-                   font    : Assets.getFont('fonts/OpenSans-Regular.ttf').fontName,
-                   italic  : true,
-                   color   : 0xFF0000,
-                   size    : 18
-               }
-        });
-        var button = UIBuilder.create(Button, {
-                text : 'HY',
-                embededFonts: true,
-                format : {
-                    font    : Assets.getFont('fonts/OpenSans-Regular.ttf').fontName,
-                }
-        });
-//        var slider = UIBuider.create(Slider, {
-//                min : -100,
-//                max : 100,
-//
+//        var vbox = UIBuilder.create(VBox);
+//        var text = UIBuilder.create(Text, {
+//              text         : 'Hello from openfl',
+//               embededFonts: true,
+//               format      : {
+//                   font    : Assets.getFont('fonts/OpenSans-Regular.ttf').fontName,
+//                   italic  : true,
+//                   color   : 0xFF0000,
+//                   size    : 18
+//               }
 //        });
-        vbox.addChild(text);
-        vbox.addChild(button);
-        //vbox.addChild(slider);
-        Lib.current.addChild(vbox);
+//        var button = UIBuilder.create(Button, {
+//                text : 'HY',
+//                embededFonts: true,
+//                format : {
+//                    font    : Assets.getFont('fonts/OpenSans-Regular.ttf').fontName,
+//                }
+//        });
+//        vbox.addChild(text);
+//        vbox.addChild(button);
+//        Lib.current.addChild(vbox);
+        //register skins
+        UIBuilder.regSkins('Assets/ui/skins.xml');
+
+        //Create our UI
+        Lib.current.addChild( UIBuilder.buildFn('Assets/ui/index.xml')() );
     }
 }
